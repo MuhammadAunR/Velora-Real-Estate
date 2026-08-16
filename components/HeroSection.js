391851bd3bde0d159a809ab22c523413f972f036
M Aun Rasheed <greataun786@gmail.com>
2026-08-16 12:05:53 +0500

    feat: added hero section

---
+++ components/HeroSection.js
@@ -0,0 +1,62 @@
+'use client'
+
+import React from 'react'
+import Button from './UI/Button'
+import Image from 'next/image'
+import { motion } from 'motion/react'
+
+
+const HeroSection = () => {
+    return (
+        <>
+            <main className='w-full h-[calc(100vh-80px)] flex items-center overflow-hidden'>
+                <section className='flex flex-col items-start gap-3 w-1/2 pl-10'>
+                    <motion.span
+                        initial={{ opacity: 0, x: -30 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.95, delay: 0.1 }}
+                        viewport={{ once: false }}
+                        className='uppercase'>Where Luxury Meets Location</motion.span>
+                    <motion.h1
+                        initial={{ opacity: 0, x: -30 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.95, delay: 0.2 }}
+                        viewport={{ once: false }}
+                        className='font-bold text-3xl lg:text-8xl'>Find Your Dream House, <br />Effortlessly</motion.h1>
+                    <motion.p
+                        initial={{ opacity: 0, x: -30 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.95, delay: 0.3 }}
+                        viewport={{ once: false }}
+                        className='max-w-xl'>Discover premium properties tailored to your lifestyle. From cozy apartments to luxury villas, we connect you with verified listings, expert agents, and a seamless buying experience — all in one place.</motion.p>
+                    <motion.span
+                        initial={{ opacity: 0, x: -30 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.95, delay: 0.4 }}
+                        viewport={{ once: false }}
+                    >
+                        <Button text={'Explore Properties'} />
+                    </motion.span>
+                </section>
+
+                <motion.div
+                    initial={{ opacity: 0, x: 30 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.95, delay: 0.1 }}
+                    viewport={{ once: false }}
+                    className="relative w-1/2 h-full">
+                    <Image
+                        src="/hero-section.jpg"
+                        alt="Luxury property"
+                        fill
+                        sizes='1200px'
+                        loading='eager'
+                        className="rounded-tl-[40%] w-full h-full object-cover shadow-[0px_10px_20px_rgb(31,42,68)]"
+                    />
+                </motion.div>
+            </main>
+        </>
+    )
+}
+
+export default HeroSection
