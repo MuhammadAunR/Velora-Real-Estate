1159f0c752466e88db34e17203f0f674ba9f152d
M Aun Rasheed <greataun786@gmail.com>
2026-08-16 15:05:46 +0500

    feat: added services section

--- components/Services.js
+++ components/Services.js
@@ -3,6 +3,8 @@
 import { MapPin, UserPen, ClipboardList, Handshake } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
+import SectionHeader from './SectionHeader'
+import { motion } from 'motion/react'
 
 const Services = () => {
 
@@ -30,50 +32,24 @@ const Services = () => {
     ]
     return (
         <>
-            <main className='min-h-screen h-fit w-full bg-radial from-secondary/50 to-white'>
-
-                <section className='flex items-center justify-between py-15'>
-                    <div className='relative h-100 w-150'>
-                        <Image
-                            src={'/services-section-image.webp'}
-                            alt='Houses'
-                            fill
-                            sizes='1000px'
-                            className='w-full h-full rounded-r-2xl shadow-2xl'
-                        />
-                    </div>
-                    <div className='flex flex-col gap-5 pr-10'>
-                        <h1 className='text-4xl lg:text-6xl font-bold font-heading'>We Help You To <br />Find Your Dream Home</h1>
-                        <p className='max-w-2xl'>From cozy cottages to luxurious estates, our dedicated team guides you through every step of the journey, ensuring your dream home becomes a reality</p>
-                        <div className='flex items-center justify-between'>
-                            <div className='flex flex-col items-start'>
-                                <span className='text-3xl leading-none font-bold'>8K+</span>
-                                <span>Houses Available</span>
-                            </div>
-                            <div className='flex flex-col items-start'>
-                                <span className='text-3xl leading-none font-bold'>8K+</span>
-                                <span>Houses Available</span>
-                            </div>
-                            <div className='flex flex-col items-start'>
-                                <span className='text-3xl leading-none font-bold'>8K+</span>
-                                <span>Houses Available</span>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+            <main id='services' className='min-h-screen h-fit w-full bg-radial from-secondary/50 to-white'>
 
                 <section>
-                    <div className='flex flex-col items-center justify-center gap-5 py-10'>
-                        <h2 className='text-4xl lg:text-6xl font-bold font-heading'>Why Choose Us</h2>
-                        <p className='max-w-2xl text-center'>Elevating Your Home Buying Experience with Expertise, Integrity, and Unmatched Personalized Service</p>
-                    </div>
+                    <SectionHeader
+                        label={'Why Choose Us'}
+                        heading={'Elevating Your Home Buying Experience'}
+                        description={'Expertise, integrity, and unmatched personalized service — every step, every detail, tailored to you.'}
+                    />
 
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 max-w-6xl mx-auto py-10">
                         {whyChooseUsData.map((data, index) => {
                             const Icon = data.icon;
-                            console.log(data.icon, Array.isArray(data.icon));
                             return (
-                                <div
+                                <motion.div
+                                    initial={{ opacity: 0, scale: 0 }}
+                                    whileInView={{ opacity: 1, scale: 1 }}
+                                    transition={{ duration: 0.10, delay: index * 0.1 }}
+                                    viewport={{ once: true }}
                                     key={data.title}
                                     className="bg-secondary/50 rounded-2xl p-6 text-left hover:-translate-y-1 transition-all duration-300 relative"
                                 >
@@ -92,10 +68,50 @@ const Services = () => {
                                     <span className="absolute top-0 right-5 text-accent/50 font-heading font-bold text-5xl">
                                         0{index + 1}
                                     </span>
-                                </div>
+                                </motion.div>
                             );
                         })}
                     </div>
+                </section>
+
+                <section className='flex flex-col lg:flex-row items-center justify-between gap-x-10 gap-y-5 py-15 max-lg:px-5 lg:w-10/12 mx-auto'>
+                    <motion.div
+                        initial={{ opacity: 0, filter: 'blur(10px)' }}
+                        whileInView={{ opacity: 1, filter: 'blur(0px)' }}
+                        transition={{ duration: 0.95 }}
+                        viewport={{ once: true }}
+                        className='relative h-130 w-full lg:h-100 lg:w-150'>
+                        <Image
+                            src={'/services-section-image.webp'}
+                            alt='Houses'
+                            fill
+                            sizes='1000px'
+                            className='w-full h-full shrink-0 rounded-2xl shadow-2xl'
+                        />
+                    </motion.div>
+                    <motion.div
+                        initial={{ opacity: 0, x: 30 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.40 }}
+                        viewport={{ once: true }}
+                        className='flex flex-col gap-5 w-full lg:w-1/2'>
+                        <h1 className='text-4xl lg:text-6xl font-bold font-heading'>We Help You To <br />Find Your Dream Home</h1>
+                        <p className='max-w-2xl'>From cozy cottages to luxurious estates, our dedicated team guides you through every step of the journey, ensuring your dream home becomes a reality</p>
+                        <div className='flex items-center justify-between'>
+                            <div className='flex flex-col items-start md:flex-wrap'>
+                                <span className='text-3xl leading-none font-bold'>8K+</span>
+                                <span>Houses Available</span>
+                            </div>
+                            <div className='flex flex-col items-start'>
+                                <span className='text-3xl leading-none font-bold'>8K+</span>
+                                <span>Houses Available</span>
+                            </div>
+                            <div className='flex flex-col items-start'>
+                                <span className='text-3xl leading-none font-bold'>8K+</span>
+                                <span>Houses Available</span>
+                            </div>
+                        </div>
+                    </motion.div>
                 </section>
             </main >
         </>
