8d94054016e8b26f916e6a3af818bfe4e21c7f9c
M Aun Rasheed <greataun786@gmail.com>
2026-08-16 16:46:23 +0500

    feat: add navOptions to constant

---
+++ constants/NavOptions.js
@@ -0,0 +1,8 @@
+const navOptions = [
+    { label: 'Home', href: '#home' },
+    { label: 'Services', href: '#services' },
+    { label: 'Gallery', href: '#gallery' },
+    { label: 'Contact Us', href: '#contact' },
+    { label: 'About Us', href: '#about' },
+]
+export { navOptions }
